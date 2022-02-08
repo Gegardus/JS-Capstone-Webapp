b4fa0dfeaa866b4cd1dfa42806900ada99fffc58
Gegardus <stgegard@gmail.com>
2022-02-08 19:03:24 +0400

    Update js to get number of likes

---
+++ src/module/interact.js
@@ -0,0 +1,16 @@
+export const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+export const appId = '5Ap1XN8WUsuZk6doKhi8';
+//export const appId = '2meYK6B3b0SbTokwpUri';
+
+export const getLikes = async () => {
+  const response = await fetch(`${baseURL}apps/${appId}/likes`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  return response.json();
+};
+
+
+
