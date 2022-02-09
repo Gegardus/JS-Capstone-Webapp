c0ab80e6283d98506a12bfc064632f284b49f23b
Gegardus <stgegard@gmail.com>
2022-02-09 17:18:41 +0400

    Add functionality to count all items

--- src/module/get-api.js
+++ src/module/get-api.js
@@ -2,7 +2,14 @@ export const getMovies = async () => {
     const response = await fetch(`https://api.tvmaze.com/shows`);
     return response.json();
   };
+
 export const getMovieById = async (id) => {
   const response = await fetch(`https://api.tvmaze.com/shows/${id}`);
   return response.json();
+};
+
+export const countMovies = (movies) => {
+  if (movies.length) {
+    document.querySelector('.count-movies').innerHTML = `${movies.length}`;
+  }
 };
